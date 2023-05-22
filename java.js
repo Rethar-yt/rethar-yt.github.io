3049663339ed7f6b10eebb3d8f70ccf51877666d
Rethar Osman Abdullah <retharyt@gmail.com>
2023-05-22 21:29:21 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,9 @@
-
+window.addEventListener('DOMContentLoaded', function() {
+    var scrollTopButton = document.getElementById('scroll-top-button');
+    
+    scrollTopButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  });
+  
