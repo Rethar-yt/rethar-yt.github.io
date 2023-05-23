6b4a0e2582059f672c30d50085d031de602730cf
Rethar Osman Abdullah <retharyt@gmail.com>
2023-05-23 19:52:08 +0200

    Delete java.js

--- java.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var scrollTopButton = document.getElementById('scroll-top-button');
-    
-    scrollTopButton.addEventListener('click', function(e) {
-      e.preventDefault();
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-  });
-  
