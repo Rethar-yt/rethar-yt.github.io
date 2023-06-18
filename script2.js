ba2e97310345155f11af24584f4dd2534279e964
Rethar Osman Abdullah <retharyt@gmail.com>
2023-06-18 20:31:11 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,9 +1,15 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var hamburgerMenu = document.querySelector('.hamburger-menu');
-    var navList = document.querySelector('nav ul');
-  
-    hamburgerMenu.addEventListener('click', function() {
-      navList.classList.toggle('open');
-    });
-  });
-  
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3500); // Change image every "" seconds
+}
+
