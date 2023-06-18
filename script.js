3a76b29bc49f5dbafeb43626ed5ea330c5883465
Rethar Osman Abdullah <retharyt@gmail.com>
2023-06-18 20:30:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-var scrollTopButton = document.getElementById('scroll-top-button');
-var scrollTimeout;
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
-window.addEventListener('scroll', function() {
-  clearTimeout(scrollTimeout);
-  scrollTimeout = setTimeout(function() {
-    scrollTopButton.style.opacity = '0'; // Hide the button after 2 seconds of inactivity
-  }, 2000);
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
   
-  if (window.pageYOffset > 0) {
-    scrollTopButton.style.opacity = '1'; // Show the button when scrolling down
-  } else {
-    scrollTopButton.style.opacity = '0'; // Hide the button when already at the top
-  }
-});
-
-scrollTopButton.addEventListener('click', function(e) {
-  e.preventDefault();
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
