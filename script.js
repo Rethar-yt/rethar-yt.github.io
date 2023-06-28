664646674a1dda04552511a049952a01e47e3968
Rethar Osman Abdullah <retharyt@gmail.com>
2023-06-29 00:24:04 +0200

    Link Text update on all html pages, included photo or rethar in push

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
   
+var scrollTopButton = document.getElementById('scroll-top-button');
+var scrollTimeout;
+
+window.addEventListener('scroll', function() {
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(function() {
+    scrollTopButton.style.opacity = '0'; 
+  }, 2000);
+  
+  if (window.pageYOffset > 0) {
+    scrollTopButton.style.opacity = '1'; 
+  } else {
+    scrollTopButton.style.opacity = '0'; 
+  }
+});
+
+scrollTopButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
