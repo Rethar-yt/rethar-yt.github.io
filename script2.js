d68df5daaed208ce6556e36eaf625da6d20f294b
Rethar Osman Abdullah <retharyt@gmail.com>
2023-07-02 12:03:55 +0200

    update slider javascript/ enable auto play plus next and back click activation/ dot activation

--- script2.js
+++ script2.js
@@ -1,15 +1,44 @@
 let slideIndex = 0;
-showSlides();
+let timer;
 
-function showSlides() {
+function startSlideshow() {
+  clearTimeout(timer);
+  timer = setTimeout(function() {
+    plusSlides(1);
+    startSlideshow();
+  }, 3000);
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = slides.length;
+  }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3500); // Change image every "" seconds
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
+
+startSlideshow();
 
